7367f8f331d7fb736ea9ca7d896c8e12165f0907
ErickHJ1 <erixkhj1@gmail.com>
2024-06-27 10:18:48 -0600

    agregue las rutas para cada pagina y añadi el server

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-
+import Login from './components/Login'
 import './App.css'
+import AboutPage from './components/AboutPage';
+import HomePage from './components/HomePage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ContactPage from './components/ContactPage';
+import Register from './components/Register';
+
 
 function App() {
-
   return (
-    <>
-     <h1>Algo</h1>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage/>}></Route>
+        <Route path="/about" element={<AboutPage/>}></Route>
+        <Route path="/contact" element={<ContactPage/>}></Route>
+        <Route path='/login' element={<Login/>} ></Route>
+        <Route path='/register' element={<Register/>}></Route>
+        </Routes>
+    </Router>
+  ); 
 }
 
 export default App
---
+++ src/components/Register.jsx
@@ -0,0 +1,12 @@
+const Register = () => {
+    return(
+        <>
+        <input type="text" />
+        <input type="text" />
+        <input type="password" />
+        <button>Registrarse</button>
+        </>
+    )
+}
+
+export default Register
